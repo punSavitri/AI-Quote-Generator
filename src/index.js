d1760c0404434cd9f6a3f628231dfd56c676c7f4
punSavitri <punshakshi@gmail.com>
2024-03-07 00:21:14 +0000

    added functionality to improved user experience

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function generateQuote(event) {
   console.log(value);
 
   let apiKey = "4c3a38t82d64bfo4330f17ff02bfbd97";
-  let context = "You are a expert AI assistant to personalised quote based on user personal interest.Your mission is to generate a short quote in basic HTML and separate line with a <br/>.Make sure answer is clear and precise . Please include author of the quotation inside a <strong> element at the bottom of the quote. Make sure to follow the user instruction.";
+  let context = "You are a expert AI assistant to personalised quote based on user personal interest.Your mission is to generate a short quote in basic HTML and separate line with a <br/>.Make sure your answer is clear and precise . Please include author's name wrap inside single quotation symbol a <strong> element at the bottom of the answer. Add - before author's name. Make sure to follow the user instruction.";
   //generating the poem based on whatever the user typed in the input field 
   let prompt = `User instruction:Generate a famous, inspirational, motivational quote about ${userInstructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
